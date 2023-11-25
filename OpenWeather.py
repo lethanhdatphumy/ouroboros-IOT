f44e3579f15f8fdec37b124fbab0fdd1ec0d9618
Đạtdy Dảk <90812330+lethanhdatphumy@users.noreply.github.com>
2023-11-25 16:14:03 +0700

    add Irrrigation.py

--- OpenWeather.py
+++ OpenWeather.py
@@ -47,4 +47,4 @@ if __name__ == "__main__":
     open_weather = OpenWeather(args.destination)
     open_weather.openWeather_Run()
 
-    print(json.dumps(weather_data, indent=4))
+
--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 from scheduler import Scheduler
 from OpenWeather import OpenWeather
 from GetHumidityByLocation import GetHumidityByLocation
+from Irrigation import IrrigationSystem
 import time
 
-global_data = ['ho chi minh', '', '', '', '']  # [temp, humanity, tưới-ed ? , giờ tưới]
+global_data = ['ho chi minh', '', '', False, False]  # [city, temp, humanity, current status  , status change ]
 
 scheduler = Scheduler()
 scheduler.SCH_Init()
 
 taskOpenWeather = OpenWeather(global_data)
 getHumidityByLocation = GetHumidityByLocation(global_data)
+irrigation_system = IrrigationSystem(global_data[0], global_data[3], global_data[4])
+
+# Adding a task to run the IrrigationSystem
+
 
 scheduler.SCH_Add_Task(taskOpenWeather.openWeather_Run, DELAY=1000, PERIOD=5000)
 scheduler.SCH_Add_Task(getHumidityByLocation.GetHumidity_Run, DELAY=1000, PERIOD=5000)
-
+scheduler.SCH_Add_Task(irrigation_system.IrrigationSystem_Run, DELAY=1000, PERIOD=5000)
 while True:
     scheduler.SCH_Update()
     scheduler.SCH_Dispatch_Tasks()
